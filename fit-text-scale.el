75f74aa14bb38ab00f184ae0a51262eaab07a27c
Marco Wahl <marcowahlsoft@gmail.com>
2020-02-17 09:44:04 +0100

    new generation

--- fit-text-scale.el
+++ fit-text-scale.el
@@ -25,7 +25,7 @@
 ;; GNU General Public License for more details.
 ;; 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -101,8 +101,7 @@
 ;;; Code:
 
 
-(require 'cl-lib) ; cl-incf
-(require 'cl-macs) ; cl-assert
+(require 'cl-lib) ; cl-incf, cl-assert
 
 
 ;; customizables
